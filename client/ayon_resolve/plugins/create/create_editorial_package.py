9f0040e73a94a981921d8a28586cb53ebdcf80ff
Jakub Jezek <jakubjezek001@gmail.com>
2024-07-29 11:31:00 +0200

    Refactor metadata update process in CreateEditorialPackage
    Simplify metadata update process by refactoring code for better readability and maintainability.

--- client/ayon_resolve/plugins/create/create_editorial_package.py
+++ client/ayon_resolve/plugins/create/create_editorial_package.py
@@ -16,22 +16,13 @@ class CreateEditorialPackage(LegacyCreator):
     def process(self):
         """Process the creation of the editorial package."""
         current_timeline = lib.get_current_timeline()
-        timeline_name = current_timeline.GetName()
 
-        # get timeline media pool item for metadata update
-        timeline_media_pool_item = None
-        for item in lib.iter_all_media_pool_clips():
-            item_name = item.GetName()
-            if item_name != timeline_name:
-                continue
-            timeline_media_pool_item = item
-            break
+        timeline_media_pool_item = lib.get_timeline_media_pool_item(
+            current_timeline
+        )
 
-        # Update the metadata
-        if timeline_media_pool_item:
-            publish_data = {"publish": self.data}
-            timeline_media_pool_item.SetMetadata(
-                lib.pype_tag_name, json.dumps(publish_data)
-            )
-        else:
-            NameError("Timeline not found in media pool.")
+        publish_data = {"publish": self.data}
+
+        timeline_media_pool_item.SetMetadata(
+            lib.pype_tag_name, json.dumps(publish_data)
+        )

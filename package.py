0974fe01b2d2aedf4138630472d559bc62cf4c39
robin <robin@ynput.io>
2024-09-11 08:50:28 -0400

    Add app_host_name and ayon_server_version.

--- package.py
+++ package.py
@@ -1,9 +1,10 @@
 name = "resolve"
 title = "DaVinci Resolve"
 version = "0.2.4-dev.1"
-
+app_host_name = "resolve"
 client_dir = "ayon_resolve"
 
+ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
     "core": ">0.3.2",
 }

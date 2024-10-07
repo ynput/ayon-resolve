c7109ff9e9a98b0e278020d5d00faff8506a9beb
robin@ynput.io <robin@ynput.io>
2024-10-07 15:05:44 -0400

    Transfer create_editorial_package to new publisher.

--- client/ayon_resolve/plugins/create/create_editorial_package.py
+++ client/ayon_resolve/plugins/create/create_editorial_package.py
@@ -1,88 +1,120 @@
 import json
 from copy import deepcopy
-from ayon_core.tools.context_dialog.window import (
-    ContextDialog,
-    ContextDialogController
-)
-from ayon_core.pipeline import get_current_project_name
-from ayon_api import get_folder_by_id, get_task_by_id, get_folder_by_path
-from ayon_core.pipeline.create.legacy_create import LegacyCreator
+
+from ayon_core.pipeline.create import CreatorError, CreatedInstance
 
 from ayon_resolve.api import lib, constants
-from ayon_resolve.api.plugin import get_editorial_publish_data
+from ayon_resolve.api.plugin import ResolveCreator, get_editorial_publish_data
 
 
-class CreateEditorialPackage(LegacyCreator):
+class CreateEditorialPackage(ResolveCreator):
     """Create Editorial Package."""
 
-    name = "editorial_pkg"
+    identifier = "io.ayon.creators.resolve.editorial_pkg"
+    product_name = "editorial_pkgMain"
     label = "Editorial Package"
     product_type = "editorial_pkg"
     icon = "camera"
     defaults = ["Main"]
 
-    def process(self):
-        """Process the creation of the editorial package."""
-        project_name = get_current_project_name()
-        folder_path = self.data["folderPath"]
-
-        current_folder = get_folder_by_path(project_name, folder_path)
-        print(current_folder)
+    def create(self, subset_name, instance_data, pre_create_data):
+        """
+        """
+        super(CreateEditorialPackage, self).create(subset_name,
+                                           instance_data,
+                                           pre_create_data)
 
         current_timeline = lib.get_current_timeline()
 
-        context = ask_for_context(
-            project_name, current_folder["id"]
-        )
-
-        if context is None:
-            return
-
-        # Get workfile path to save to.
-        project_name = context["project_name"]
-        folder = get_folder_by_id(project_name, context["folder_id"])
-
-        # task is optional so we need to check if it is set
-        task = None
-        if "task_id" in context:
-            task = get_task_by_id(project_name, context["task_id"])
-
-        # reset self.data to be pointing in the set context data
-        self.data["folderPath"] = folder["path"]
-
-        if task:
-            self.data.update({
-                "taskId": task["id"],
-                "taskName": task["name"],
-            })
-
         if not current_timeline:
-            raise RuntimeError("Make sure to have an active current timeline.")
+            raise CreatorError("Make sure to have an active current timeline.")
 
         timeline_media_pool_item = lib.get_timeline_media_pool_item(
             current_timeline
         )
 
-        publish_data = deepcopy(self.data)
+        publish_data = deepcopy(instance_data)
+
         # add publish data for streamline publishing
         publish_data["publish"] = get_editorial_publish_data(
-            folder_path=folder["path"],
-            product_name=self.data["productName"],
+            folder_path=instance_data["folderPath"],
+            product_name=self.product_name,
         )
 
+        publish_data["label"] = current_timeline.GetName()
         timeline_media_pool_item.SetMetadata(
             constants.AYON_TAG_NAME, json.dumps(publish_data)
         )
 
+        publish_data["media_pool_item_id"] = timeline_media_pool_item.GetUniqueId()
+        new_instance = CreatedInstance(
+            self.product_type,
+            self.product_name,
+            publish_data,
+            self,
+        )
+        new_instance.transient_data["timeline_item"] = timeline_media_pool_item
+        self._add_instance_to_context(new_instance)
 
-def ask_for_context(
-    project_name, folder_id
-):
-    """Ask for context to create Editorial Package."""
-    controller = ContextDialogController()
-    window = ContextDialog(controller=controller)
-    controller.set_expected_selection(
-        project_name, folder_id)
-    window.exec_()
+    def collect_instances(self):
+        """Collect all created instances from current timeline."""
+        for media_pool_item in lib.iter_all_media_pool_clips():
+            data = media_pool_item.GetMetadata(constants.AYON_TAG_NAME)
+            if not data:
+                continue
 
-    return controller.get_selected_context()
+            try:
+                data = json.loads(data)
+            except json.JSONDecodeError:
+                self.log.warning(
+                    f"Failed to parse json data from media pool item: "
+                    f"{media_pool_item.GetName()}"
+                )
+                continue
+
+            # exclude all which are not productType editorial_pkg
+            if (
+                data.get("publish", {}).get("productType") != "editorial_pkg"
+            ):
+                continue
+
+            data["media_pool_item_id"] = media_pool_item.GetUniqueId()
+            current_instance = CreatedInstance(
+                self.product_type,
+                self.product_name,
+                data,
+                self
+            )
+
+            current_instance.transient_data["timeline_item"] = media_pool_item            
+            self._add_instance_to_context(current_instance)
+
+    def update_instances(self, update_list):
+        """Store changes of existing instances so they can be recollected.
+
+        Args:
+            update_list(List[UpdateData]): Gets list of tuples. Each item
+                contain changed instance and it's changes.
+        """
+        for created_inst, _changes in update_list:
+            timeline_media_pool_item = created_inst.transient_data["timeline_item"]
+            timeline_media_pool_item.SetMetadata(
+                constants.AYON_TAG_NAME,
+                json.dumps(created_inst.data_to_store()),
+            )
+
+    def remove_instances(self, instances):
+        """Remove instance marker from track item.
+
+        Args:
+            instance(List[CreatedInstance]): Instance objects which should be
+                removed.
+        """
+        for instance in instances:
+            self._remove_instance_from_context(instance)
+            timeline_media_pool_item = instance.transient_data["timeline_item"]
+            timeline_media_pool_item.SetMetadata(
+                constants.AYON_TAG_NAME,
+                json.dumps({}),
+            )
+
--- client/ayon_resolve/plugins/publish/collect_editorial_package.py
+++ client/ayon_resolve/plugins/publish/collect_editorial_package.py
@@ -1,88 +1,56 @@
-import json
-
 import pyblish.api
 
 import ayon_api
-from ayon_api import get_task_by_id
 
 from ayon_resolve.api import lib, constants
 
 
-class EditorialPackageInstances(pyblish.api.ContextPlugin):
+class EditorialPackageInstances(pyblish.api.InstancePlugin):
     """Collect all Track items selection."""
 
     order = pyblish.api.CollectorOrder - 0.49
     label = "Collect Editorial Package Instances"
     families = ["editorial_pkg"]
 
-    def process(self, context):
-        project_name = context.data["projectName"]
+    def process(self, instance):
+        project_name = instance.context.data["projectName"]
         self.log.info(f"project: {project_name}")
 
+        media_pool_item_id = instance.data["media_pool_item_id"]
         for media_pool_item in lib.iter_all_media_pool_clips():
+            if media_pool_item.GetUniqueId() == media_pool_item_id:
+                break
+        else:
+            raise RuntimeError("Could not identify media pool item from instance.")
 
-            data = media_pool_item.GetMetadata(constants.AYON_TAG_NAME)
-            if not data:
-                continue
+        # get version from publish data and rise it one up
+        version = instance.data.get("version")
+        if version is not None:
+            version += 1
 
-            try:
-                data = json.loads(data)
-            except json.JSONDecodeError:
-                self.log.warning(
-                    f"Failed to parse json data from media pool item: "
-                    f"{media_pool_item.GetName()}"
-                )
-                continue
-
-            # exclude all which are not productType editorial_pkg
-            if (
-                data.get("publish")
-                and data["publish"].get("productType") != "editorial_pkg"
-            ):
-                continue
-
-            instance = context.create_instance(name=media_pool_item.GetName())
-
-            publish_data = data["publish"]
-
-            # get version from publish data and rise it one up
-            version = publish_data.get("version")
-            if version is not None:
-                version += 1
-
-                # make sure last version of product is higher than current
-                # expected current version from publish data
-                folder_entity = ayon_api.get_folder_by_path(
-                    project_name=project_name,
-                    folder_path=publish_data["folderPath"],
-                )
-                last_version = ayon_api.get_last_version_by_product_name(
-                    project_name=project_name,
-                    product_name=publish_data["productName"],
-                    folder_id=folder_entity["id"],
-                )
-                if last_version is not None:
-                    last_version = int(last_version["version"])
-                    if version <= last_version:
-                        version = last_version + 1
-
-                publish_data["version"] = version
-
-            publish_data.update(
-                {
-                    "mediaPoolItem": media_pool_item,
-                    "item": media_pool_item,
-                }
+            # make sure last version of product is higher than current
+            # expected current version from publish data
+            folder_entity = ayon_api.get_folder_by_path(
+                project_name=project_name,
+                folder_path=publish_data["folderPath"],
             )
+            last_version = ayon_api.get_last_version_by_product_name(
+                project_name=project_name,
+                product_name=publish_data["productName"],
+                folder_id=folder_entity["id"],
+            )
+            if last_version is not None:
+                last_version = int(last_version["version"])
+                if version <= last_version:
+                    version = last_version + 1
 
-            if publish_data.get("taskId"):
-                task_entity = get_task_by_id(
-                    project_name=project_name,
-                    task_id=publish_data["taskId"],
-                )
-                publish_data["taskEntity"] = task_entity
-                publish_data["task"] = task_entity["name"]
+            instance.data["version"] = version
 
-            instance.data.update(publish_data)
+        instance.data.update(
+            {
+                "mediaPoolItem": media_pool_item,
+                "item": media_pool_item,
+            }
+        )
 
-            self.log.info(f"Editorial Package: {instance.data}")
+        self.log.info(f"Editorial Package: {instance.data}")

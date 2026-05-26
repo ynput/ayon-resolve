bfe140f01508c91f01d29766a7d8059ab37d9ef8
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-05-26 17:00:27 +0200

    handle workfile path in pre-launch hook

--- client/ayon_resolve/hooks/pre_resolve_last_workfile.py
+++ client/ayon_resolve/hooks/pre_resolve_last_workfile.py
@@ -13,23 +13,33 @@ class PreLaunchResolveLastWorkfile(PreLaunchHook):
     launch_types = {LaunchTypes.local}
 
     def execute(self):
-        if not self.data.get("start_last_workfile"):
-            self.log.info("It is set to not start last workfile on start.")
-            return
-
-        last_workfile = self.data.get("last_workfile_path")
-        if not last_workfile:
-            self.log.warning("Last workfile was not collected.")
-            return
-
-        if not os.path.exists(last_workfile):
-            self.log.info("Current context does not have any workfile yet.")
+        workfile_path = self.get_workfile_path()
+        if not workfile_path:
             return
 
         # Add path to launch environment for the startup script to pick up
         self.log.info(
             "Setting AYON_RESOLVE_OPEN_ON_LAUNCH to launch "
-            f"last workfile: {last_workfile}"
+            f"last workfile: {workfile_path}"
         )
         key = "AYON_RESOLVE_OPEN_ON_LAUNCH"
-        self.launch_context.env[key] = last_workfile
+        self.launch_context.env[key] = workfile_path
+
+    def get_workfile_path(self):
+        workfile_path = self.data.get("workfile_path")
+        if workfile_path:
+            return workfile_path
+
+        if not self.data.get("start_last_workfile"):
+            self.log.info("It is set to not start last workfile on start.")
+            return None
+
+        last_workfile = self.data.get("last_workfile_path")
+        if not last_workfile:
+            self.log.warning("Last workfile was not collected.")
+            return None
+
+        if not os.path.exists(last_workfile):
+            self.log.info("Current context does not have any workfile yet.")
+            return None
+        return last_workfile
